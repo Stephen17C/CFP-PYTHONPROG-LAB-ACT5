93aa9da208e1e7df162351fa602b75ae7c640181
Stephen17C <markstephenseril@gmail.com>
2025-03-12 00:15:46 +0800

    Add files via upload

---
+++ Act lab 5.py
@@ -0,0 +1,17 @@
+classify_age = int(input(" How old are you?"))
+if classify_age >=100:
+	print("you are a centenarian.")
+elif classify_age >= 70:
+	print("you are a senior citizen")
+elif classify_age >= 50:
+	print("you are a middle age")
+elif classify_age >= 30:
+	print("you are a tricenarian")
+elif classify_age >= 25:
+	print("you are a young adults")
+elif classify_age >= 18:
+	print("you are a adolescene")
+elif classify_age >= 2:
+	print("you are a baby")
+else:
+	print("This is age categories.")
